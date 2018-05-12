3b673bf16cc7557339ed21d51153a52bbbadd5ef
sunshijie <1029783071@qq.com>
2018-05-12 17:23:28 +0800

    copy from mot chanllenge

---
+++ demo_evalMOT16.m
@@ -0,0 +1,3 @@
+benchmarkGtDir = '../MOT16Labels/train/';
+[allMets, metsBenchmark] = evaluateTracking('c5-train.txt', 'res/MOT16/data/', benchmarkGtDir, 'MOT16');
+
---
+++ demo_evalMOT17.m
@@ -0,0 +1,2 @@
+benchmarkGtDir = '../MOT17Labels/train/';
+[allMets, metsBenchmark] = evaluateTracking('custom_test.txt', 'res/MOT17/data/', benchmarkGtDir, 'MOT17');
